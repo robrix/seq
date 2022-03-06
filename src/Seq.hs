0d71af670062b8b447b8e664d2149446d11e9c9b
Rob Rix <rob.rix@me.com>
2022-03-06 09:38:20 -0500

    Export Print.

--- src/Seq.hs
+++ src/Seq.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
 module Seq
 ( Seq(..)
+, Print(..)
 ) where
 
 class Seq term coterm command | term -> coterm command, coterm -> term command, command -> term coterm where
