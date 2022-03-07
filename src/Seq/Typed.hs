1f7f5f06654798187887a067c258d4ecf41648ee
Rob Rix <rob.rix@me.com>
2022-03-07 07:20:55 -0500

    Introduce product terms.

--- src/Seq/Typed.hs
+++ src/Seq/Typed.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FunctionalDependencies #-}
 module Seq.Typed
-( Seq
+( Seq(..)
 ) where
 
 class Seq term coterm command | term -> coterm command, coterm -> term command, command -> term coterm where
+  prdR :: term a -> term b -> term (a, b)
