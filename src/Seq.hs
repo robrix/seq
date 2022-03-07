685ad65eb7b187bb6b49d0b1f94ce9408595d9d1
Rob Rix <rob.rix@me.com>
2022-03-07 07:17:24 -0500

    Move Seq to the untyped module.

--- src/Seq.hs
+++ src/Seq.hs
@@ -1,21 +1,5 @@
-{-# LANGUAGE FunctionalDependencies #-}
 module Seq
-( Seq(..)
+( module Seq.Untyped
 ) where
 
-class Seq term coterm command | term -> coterm command, coterm -> term command, command -> term coterm where
-  -- right rules
-  prdR :: term -> term -> term
-  sumR1 :: term -> term
-  sumR2 :: term -> term
-  funR :: (term -> coterm -> command) -> term
-
-  -- left rules
-  prdL1 :: coterm -> coterm
-  prdL2 :: coterm -> coterm
-  sumL :: coterm -> coterm -> coterm
-  funL :: term -> coterm -> coterm
-
-  (.|.) :: term -> coterm -> command
-
-  infix 1 .|.
+import Seq.Untyped
--- src/Seq/Untyped.hs
+++ src/Seq/Untyped.hs
@@ -1,2 +1,21 @@
+{-# LANGUAGE FunctionalDependencies #-}
 module Seq.Untyped
-() where
+( Seq(..)
+) where
+
+class Seq term coterm command | term -> coterm command, coterm -> term command, command -> term coterm where
+  -- right rules
+  prdR :: term -> term -> term
+  sumR1 :: term -> term
+  sumR2 :: term -> term
+  funR :: (term -> coterm -> command) -> term
+
+  -- left rules
+  prdL1 :: coterm -> coterm
+  prdL2 :: coterm -> coterm
+  sumL :: coterm -> coterm -> coterm
+  funL :: term -> coterm -> coterm
+
+  (.|.) :: term -> coterm -> command
+
+  infix 1 .|.
