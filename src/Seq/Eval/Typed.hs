e4ad40533ba6309dcbb28ea22dd95371754455c7
Rob Rix <rob.rix@me.com>
2022-03-07 07:49:08 -0500

    Define an Applicative instance for Eval.

--- src/Seq/Eval/Typed.hs
+++ src/Seq/Eval/Typed.hs
@@ -2,6 +2,11 @@
 module Seq.Eval.Typed
 ( Eval(..)
 ) where
+import Data.Coerce (coerce)
 
 newtype Eval a = Eval { eval :: a }
   deriving (Functor)
+
+instance Applicative Eval where
+  pure = Eval
+  (<*>) = coerce
