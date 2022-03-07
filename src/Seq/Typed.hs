fb8e7502ea282795d816e2143f3ae81da9241953
Rob Rix <rob.rix@me.com>
2022-03-07 07:21:34 -0500

    Introduce product coterms.

--- src/Seq/Typed.hs
+++ src/Seq/Typed.hs
@@ -5,3 +5,6 @@ module Seq.Typed
 
 class Seq term coterm command | term -> coterm command, coterm -> term command, command -> term coterm where
   prdR :: term a -> term b -> term (a, b)
+
+  prdL1 :: coterm a -> coterm (a, b)
+  prdL2 :: coterm b -> coterm (a, b)
