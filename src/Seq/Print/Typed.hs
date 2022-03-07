5acc5e931b090fac83cbbc45e8e400ab85be5bd1
Rob Rix <rob.rix@me.com>
2022-03-07 07:32:15 -0500

    Define a typed Print newtype.

--- src/Seq/Print/Typed.hs
+++ src/Seq/Print/Typed.hs
@@ -1,5 +1,12 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Seq.Print.Typed
 ( Prec(..)
+, Print(..)
 ) where
 
+import Seq.Doc
+
 newtype Prec = Prec Int
+
+newtype Print a = Print { getPrint :: Prec -> Doc }
+  deriving (Monoid, Semigroup)
