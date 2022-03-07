c33a259062ee668440ebef2b3e034f340f54afba
Rob Rix <rob.rix@me.com>
2022-03-07 07:22:42 -0500

    Define sum introductions.

--- src/Seq/Typed.hs
+++ src/Seq/Typed.hs
@@ -6,6 +6,8 @@ module Seq.Typed
 class Seq term coterm command | term -> coterm command, coterm -> term command, command -> term coterm where
   -- right rules
   prdR :: term a -> term b -> term (a, b)
+  sumR1 :: term a -> term (Either a b)
+  sumR2 :: term b -> term (Either a b)
 
   -- left rules
   prdL1 :: coterm a -> coterm (a, b)
