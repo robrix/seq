034b0a548305082be171838b430ba63e5f132742
Rob Rix <rob.rix@me.com>
2022-03-07 07:21:51 -0500

    :memo: the sections.

--- src/Seq/Typed.hs
+++ src/Seq/Typed.hs
@@ -4,7 +4,9 @@ module Seq.Typed
 ) where
 
 class Seq term coterm command | term -> coterm command, coterm -> term command, command -> term coterm where
+  -- right rules
   prdR :: term a -> term b -> term (a, b)
 
+  -- left rules
   prdL1 :: coterm a -> coterm (a, b)
   prdL2 :: coterm b -> coterm (a, b)
