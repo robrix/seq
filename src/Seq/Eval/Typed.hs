87f8b6ecaaeb8d9452e7bc33ce4c5c3a58a25aa0
Rob Rix <rob.rix@me.com>
2022-03-08 10:55:52 -0500

    Define Term as Cont instead of Codensity.

--- src/Seq/Eval/Typed.hs
+++ src/Seq/Eval/Typed.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DeriveFunctor #-}
-{-# LANGUAGE RankNTypes #-}
 module Seq.Eval.Typed
 ( evalTerm
 , Term(..)
@@ -7,15 +6,15 @@ module Seq.Eval.Typed
 
 import Control.Monad (ap)
 
-evalTerm :: Term a -> a
+evalTerm :: Term a a -> a
 evalTerm (Term r) = r id
 
-newtype Term a = Term { eval :: forall r . (a -> r) -> r }
+newtype Term r a = Term { eval :: (a -> r) -> r }
   deriving (Functor)
 
-instance Applicative Term where
+instance Applicative (Term r) where
   pure a = Term (\ k -> k a)
   (<*>) = ap
 
-instance Monad Term where
+instance Monad (Term r) where
   Term m >>= f = Term (\ k -> m (\ a -> eval (f a) k))
