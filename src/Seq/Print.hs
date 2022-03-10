d8a847b364057f898c933002c7c3b523286a633e
Rob Rix <rob.rix@me.com>
2022-03-09 23:22:52 -0500

    Generalize Print over the precedence type.

--- src/Seq/Print.hs
+++ src/Seq/Print.hs
@@ -7,6 +7,7 @@ module Seq.Print
 , atom
 , prec
 , withPrec
+, resetPrec
 , ($$)
 ) where
 
@@ -16,65 +17,77 @@ import Seq.Doc
 newtype Prec = Prec Int
   deriving (Bounded, Enum, Eq, Num, Ord)
 
-newtype Print r a = Print { getPrint :: Prec -> Bind }
+newtype Print prec r a = Print { getPrint :: prec -> Bind }
   deriving (Monoid, Semigroup)
 
-instance Show (Print r a) where
+instance Show (Print Prec r a) where
   showsPrec d p = string (getDoc (getBind (getPrint p (Prec d)) (Var 0)))
 
-instance Document (Print r a) where
+instance Document (Print prec r a) where
   char = Print . const . char
 
-instance Seq Print Print (Print ()) where
-  µR f = prec 0 (char 'µ' <+> bind (\ a -> brackets (var a) <+> dot <+> withPrec 0 (f (atom (var a)))))
-  prdR l r = atom (tupled [withPrec 11 l, withPrec 11 r])
-  coprdR1 l = prec 10 (str "inl" <+> withPrec 11 l)
-  coprdR2 r = prec 10 (str "inr" <+> withPrec 11 r)
-  pairR l r = atom (list [withPrec 11 l, withPrec 11 r])
+instance Seq (Print Prec) (Print Prec) (Print Prec ()) where
+  µR f = prec mu (char 'µ' <+> bind (\ a -> list [var a] <+> dot <+> resetPrec (f (atom (var a)))))
+  prdR l r = atom (tupled [resetPrec l, resetPrec r])
+  coprdR1 l = str "inl" $$ l
+  coprdR2 r = str "inr" $$ r
+  pairR l r = atom (list [resetPrec l, resetPrec r])
   notR c = prec 11 (char '¬' <+> withPrec 12 c)
-  funR f = prec 0 (char 'λ' <+> bind (\ a -> bind (\ b -> brackets (var a <> comma <+> var b) <+> dot <+> withPrec 0 (f (atom (var a)) (atom (var b))))))
+  funR f = prec lambda (char 'λ' <+> bind (\ a -> bind (\ b -> list [var a, var b] <+> dot <+> resetPrec (f (atom (var a)) (atom (var b))))))
   cofunR a b = prec 1 (withPrec 2 b <+> char '⤚' <+> withPrec 2 a)
 
-  µL f = prec 0 (str "µ̃" <+> bind (\ a -> brackets (var a) <+> dot <+> withPrec 0 (f (atom (var a)))))
-  prdL1 f = prec 10 (str "exl" <+> withPrec 11 f)
-  prdL2 f = prec 10 (str "exr" <+> withPrec 11 f)
-  coprdL l r = prec 10 (str "exlr" <+> withPrec 11 l <+> withPrec 11 r)
-  pairL f = prec 0 (str "µ̃" <> bind (\ a -> bind (\ b -> list [var a, var b] <+> dot <+> withPrec 0 (f (atom (var a)) (atom (var b))))))
-  copairL a b = atom (list [withPrec 0 a, withPrec 0 b])
-  notL t = prec 10 (str "not" <+> brackets (withPrec 11 t))
-  funL = assocr 10 dot
-  cofunL f = prec 10 (str "coapp" <+> bind (\ a -> bind (\ b -> brackets (var a <> comma <+> var b) <+> dot <+> withPrec 0 (f (atom (var a)) (atom (var b))))))
+  µL f = prec mu (str "µ̃" <+> bind (\ a -> list [var a] <+> dot <+> resetPrec (f (atom (var a)))))
+  prdL1 f = str "exl" $$ f
+  prdL2 f = str "exr" $$ f
+  coprdL l r = str "exlr" $$ l $$ r
+  pairL f = prec mu (str "µ̃" <> bind (\ a -> bind (\ b -> list [var a, var b] <+> dot <+> withPrec 0 (f (atom (var a)) (atom (var b))))))
+  copairL a b = atom (list [resetPrec a, resetPrec b])
+  notL t = str "not" $$ brackets t
+  funL = assocr ap dot
+  cofunL f = prec ap (str "coapp" <+> bind (\ a -> bind (\ b -> list [var a, var b] <+> dot <+> resetPrec (f (atom (var a)) (atom (var b))))))
 
-  t .|. c = prec 0 (withPrec 1 t <+> str "║" <+> withPrec 1 c)
+  t .|. c = prec cmd (withPrec 1 t <+> str "║" <+> withPrec 1 c)
 
 
-atom :: Bind -> Print r a
+atom :: Bind -> Print prec r a
 atom = Print . const
 
-prec :: Prec -> Bind -> Print r a
+prec :: Ord prec => prec -> Bind -> Print prec r a
 prec i b = Print (\ i' -> parensIf (i' > i) b)
 
-withPrec :: Prec -> Print r a -> Bind
+withPrec :: prec -> Print prec r a -> Bind
 withPrec = flip getPrint
 
-($$) :: Print r a -> Print r b -> Print r c
-($$) = assocl (Prec 10) space
+resetPrec :: Enum prec => Print prec r a -> Bind
+resetPrec = withPrec (toEnum 0)
+
+($$) :: Print Prec r a -> Print Prec r b -> Print Prec r c
+($$) = assocl ap space
 
 infixl 9 $$
 
 
+ap, cmd, lambda, mu :: Prec
+ap = Prec 10
+cmd = Prec 0
+lambda = Prec 0
+mu = Prec 0
+
+
 assocl
-  :: Prec      -- ^ precedence
-  -> Bind      -- ^ operator
-  -> Print r a -- ^ left operand
-  -> Print r b -- ^ right operand
-  -> Print r c
+  :: (Enum prec, Ord prec)
+  => prec           -- ^ precedence
+  -> Bind           -- ^ operator
+  -> Print prec r a -- ^ left operand
+  -> Print prec r b -- ^ right operand
+  -> Print prec r c
 assocl p o l r = prec p (surround o (withPrec p l) (withPrec (succ p) r))
 
 assocr
-  :: Prec      -- ^ precedence
+  :: (Enum prec, Ord prec)
+  => prec      -- ^ precedence
   -> Bind      -- ^ operator
-  -> Print r a -- ^ left operand
-  -> Print r b -- ^ right operand
-  -> Print r c
+  -> Print prec r a -- ^ left operand
+  -> Print prec r b -- ^ right operand
+  -> Print prec r c
 assocr p o l r = prec p (surround o (withPrec (succ p) l) (withPrec p r))
