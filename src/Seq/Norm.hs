047aea689dc25b45e988e130ed24b94061ff5bbf
Rob Rix <rob.rix@me.com>
2022-07-31 16:21:25 -0400

    Define de Bruijn names.

--- src/Seq/Norm.hs
+++ src/Seq/Norm.hs
@@ -1,2 +1,10 @@
 module Seq.Norm
-() where
+( -- * De Bruijn names
+  Level(..)
+, Index(..)
+) where
+
+-- De Bruijn names
+
+newtype Level = Level { getLevel :: Int }
+newtype Index = Index { getIndex :: Int }
