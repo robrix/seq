18ff602b94d9b808a83d5a6fb448dfcfef91b3c1
Rob Rix <rob.rix@me.com>
2022-07-31 16:24:10 -0400

    Define values, continuations, and commands.

--- src/Seq/Norm.hs
+++ src/Seq/Norm.hs
@@ -2,9 +2,35 @@ module Seq.Norm
 ( -- * De Bruijn names
   Level(..)
 , Index(..)
+  -- * Values
+, Value(..)
+  -- * Continuations
+, Continuation(..)
+  -- * Commands
+, Command(..)
 ) where
 
 -- De Bruijn names
 
 newtype Level = Level { getLevel :: Int }
 newtype Index = Index { getIndex :: Int }
+
+
+-- Values
+
+data Value
+  = Var Level
+  | Mu (Continuation -> Command)
+
+
+-- Continuations
+
+data Continuation
+  = Covar Level
+  | Comu (Value -> Command)
+
+
+-- Commands
+
+data Command
+  = Value :|: Continuation
