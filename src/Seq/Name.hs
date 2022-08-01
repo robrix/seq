83c29ac5852f05d8c8479ba554c4927f268a2c8c
Rob Rix <rob.rix@me.com>
2022-08-01 08:31:02 -0400

    Define names and variables.

--- src/Seq/Name.hs
+++ src/Seq/Name.hs
@@ -2,9 +2,25 @@ module Seq.Name
 ( -- * De Bruijn names
   Level(..)
 , Index(..)
+  -- * Names
+, Name
+  -- * Variables
+, Var(..)
 ) where
 
 -- De Bruijn names
 
 newtype Level = Level { getLevel :: Int }
 newtype Index = Index { getIndex :: Int }
+
+
+-- Names
+
+type Name = String
+
+
+-- Variables
+
+data Var a
+  = Bound a
+  | Free Name
