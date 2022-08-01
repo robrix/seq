c93d8ef9956f24223f2a479332f2bc97849371b4
Rob Rix <rob.rix@me.com>
2022-08-01 09:05:00 -0400

    Rename an import.

--- src/Seq/Untyped/Norm.hs
+++ src/Seq/Untyped/Norm.hs
@@ -12,7 +12,7 @@ module Seq.Untyped.Norm
 , C(..)
 ) where
 
-import qualified Seq.Class as SQ
+import qualified Seq.Class as Typed
 import           Seq.Name
 
 -- Values
@@ -71,63 +71,63 @@ newtype V r a = V { getV :: Value }
 newtype K r a = K { getK :: Continuation }
 newtype C r = C { getC :: Command }
 
-instance SQ.Mu V K C where
+instance Typed.Mu V K C where
   µR f = V (MuR (getC . f . K))
   µL f = K (MuL (getC . f . V))
 
-instance SQ.Command V K C where
+instance Typed.Command V K C where
   V v .|. K k = C (v :|: k) -- FIXME: this is wrong; it needs to normalize
 
 
 -- Positive
 
-instance SQ.Coprd V K C where
+instance Typed.Coprd V K C where
   coprdR1 = V . CoprdR1 . getV
   coprdR2 = V . CoprdR2 . getV
   coprdL l r = K (CoprdL (getC . l . V) (getC . r . V))
 
-instance SQ.Pair V K C where
+instance Typed.Pair V K C where
   pairR (V a) (V b) = V (PairR a b)
   pairL f = K (PairL (\ a b -> getC (f (V a) (V b))))
 
-instance SQ.Cofun V K C where
+instance Typed.Cofun V K C where
   cofunR (V v) (K k) = V (CofunR v k)
   cofunL f = K (CofunL (\ v k -> getC (f (V v) (K k))))
 
-instance SQ.Zero K where
+instance Typed.Zero K where
   zeroL = K ZeroL
 
-instance SQ.One V K C where
+instance Typed.One V K C where
   oneR = V OneR
   oneL = K . OneL . getC
 
-instance SQ.Negate V K C where
+instance Typed.Negate V K C where
   negateR (K k) = V (NegateR k)
   negateL f = K (NegateL (getC . f . K))
 
 
 -- Negative
 
-instance SQ.Prd V K C where
+instance Typed.Prd V K C where
   prdR l r = V (PrdR (getC . l . K) (getC . r . K))
   prdL1 = K . PrdL1 . getK
   prdL2 = K . PrdL2 . getK
 
-instance SQ.Copair V K C where
+instance Typed.Copair V K C where
   copairR f = V (CopairR (\ l r -> getC (f (K l) (K r))))
   copairL (K l) (K r) = K (CopairL l r)
 
-instance SQ.Fun V K C where
+instance Typed.Fun V K C where
   funR f = V (FunR (\ v k -> getC (f (V v) (K k))))
   funL (V v) (K k) = K (FunL v k)
 
-instance SQ.Top V where
+instance Typed.Top V where
   topR = V TopR
 
-instance SQ.Bottom V K C where
+instance Typed.Bottom V K C where
   bottomR = V . BottomR . getC
   bottomL = K BottomL
 
-instance SQ.Not V K C where
+instance Typed.Not V K C where
   notR f = V (NotR (getC . f . V))
   notL (V v) = K (NotL v)
