8afca99c0855fcbb88405813d864616276e34ec4
Rob Rix <rob.rix@me.com>
2022-08-01 10:17:03 -0400

    Stub in polarized (co)values.

--- src/Seq/Untyped/Polarized.hs
+++ src/Seq/Untyped/Polarized.hs
@@ -1,2 +1,40 @@
 module Seq.Untyped.Polarized
-() where
+( -- * Values
+  NValue(..)
+, PValue(..)
+  -- * Continuations
+, NContinuation(..)
+, PContinuation(..)
+  -- * Commands
+, Command(..)
+) where
+
+import Seq.Name
+
+-- Values
+
+data NValue
+  = NVarR Level
+  | NMuR (NContinuation -> Command)
+
+data PValue
+  = PVarR Level
+  | PMuR (PContinuation -> Command)
+
+
+-- Continuations
+
+data NContinuation
+  = NVarL Level
+  | NMuL (NValue -> Command)
+
+data PContinuation
+  = PVarL Level
+  | PMuL (PValue -> Command)
+
+
+-- Commands
+
+data Command
+  = NValue :|:- NContinuation
+  | PValue :|:+ PContinuation
